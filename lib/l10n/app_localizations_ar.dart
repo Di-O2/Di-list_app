7596a6b54fca9eeada2bfbedf55d20b24b4c8bcf
KHALID <WOO@HOTMAIL.com>
2026-09-12 04:20:23 +0300

    feat: add localization dictionaries and resolve intl conflict

---
+++ lib/l10n/app_localizations_ar.dart
@@ -0,0 +1,22 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Arabic (`ar`).
+class AppLocalizationsAr extends AppLocalizations {
+  AppLocalizationsAr([String locale = 'ar']) : super(locale);
+
+  @override
+  String get appTitle => 'دي-لست';
+
+  @override
+  String get tasks => 'المهام';
+
+  @override
+  String get addTask => 'إضافة مهمة';
+
+  @override
+  String get noTasks => 'لا توجد مهام بعد!';
+}
